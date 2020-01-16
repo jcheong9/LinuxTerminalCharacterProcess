734659ad2d9dd1301643219ed2b2cb7b1b73e9db
jcheoi <jcheoi@localhost.localdomain>
2020-01-15 20:17:12 -0800

    testing

--- asn1.c
+++ asn1.c
@@ -9,29 +9,47 @@
 #include <stdlib.h>
 #include <string.h>
 
+int inputProcess(char ch);
+
 int main(void) 
 {
 	pid_t childpid = 0; 
    	int i, n;
 	char ch = 'k';
 	char buf[] = "";
- 
-    	system("stty raw igncr -echo");
+
    	n = 3;  
    	for (i = 1; i < n; i++)
-      		if ((childpid = fork()) <= 0)
+      		if ((childpid = fork()) > 0)
          		break;
-	
+
+	//f((long)childpid == 0){
+		system("stty raw igncr -echo");
+		while(ch !='T'){
+			scanf("%c",&ch);
+			strncat(buf, &ch, 1); 
+			
+		} 
+		system("stty -raw -igncr echo");
+		printf("Appended String: %s\n", buf); 	
+	//}
+
+   	fprintf(stderr, "i:%d  process ID:%ld  parent ID:%ld  child ID:%ld\n",
+           i, (long)getpid(), (long)getppid(), (long)childpid);
+
+   	return 0; 
+}
+
+int inputProcess(char ch){
+
+	char buf[] = "";
+
 	while(ch !='T'){
 		scanf("%c",&ch);
 		strncat(buf, &ch, 1); 
 		
 	} 
-	
-	printf("Appended String: %s\n", buf); 
 
-   	fprintf(stderr, "i:%d  process ID:%ld  parent ID:%ld  child ID:%ld\n",
-           i, (long)getpid(), (long)getppid(), (long)childpid);
-	system("stty -raw -igncr echo");
-   	return 0; 
+	printf("Appended String: %s\n", buf); 	
+	return 0;
 }
