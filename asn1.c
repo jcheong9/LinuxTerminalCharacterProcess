894c7002ebacace8d9877bd93ad347ac146188ef
jc <jc@localhost.localdomain>
2020-01-17 19:04:11 -0800

    stuff

--- asn1.c
+++ asn1.c
@@ -6,23 +6,36 @@
 #include <signal.h>
 #include <string.h>
 
-#define MSGSIZE 			256
+#define MSGSIZE 		256
 #define abNormTermination 	0x0B
 #define endOfString   		00
 
 /*------ Function prototypes ----------*/
-void inputProcess(int p[]);
+void inputProcess(int p[], pid_t pid, pid_t ppid);
 void outputProcess(int p[]);
 void translateProcess(int p[]);
 void parent (int p[]);
 void child (int p[]);
 void fatal (char *);
+void sig_usr(int signo);
 
+static int received = 0;
 
+void readUsual(int sig)
+{
+    if (sig == SIGUSR1)
+    {
+        received = 1;
+    }
+}
 
 int main (void)
 {
 	int pfd[2];
+	pid_t pid;
+	pid_t ppid = getpid();
+
+	signal(SIGUSR1,readUsual);
 
 	/*----- Open the pipe -----------*/
 	if (pipe(pfd) < 0)
@@ -35,20 +48,22 @@ int main (void)
 	if (fcntl (pfd[0], F_SETFL, O_NDELAY) < 0)
 	  	fatal ("fcntl call");
 
+
 	/*-------- fork ---------------*/
-	switch(fork())
+	pid = fork(); 
+	switch(pid)
 	{
 		case -1:        /* error */
 			fatal ("fork call");
 		case 0:        /* It's the child */
-		  	child (pfd);
+		  	outputProcess (pfd);
 		default:       /* parent */
-		  	parent (pfd);
+		  	inputProcess (pfd, pid, ppid);
 	}
 }
 
-/*------- Parent Code -------*/
-void parent (int p[2])
+
+void inputProcess (int p[2],pid_t pid,pid_t ppid)
 {
 	char readInput;
 	char buf[MSGSIZE];
@@ -66,7 +81,16 @@ void parent (int p[2])
 				write (p[1], buf, MSGSIZE);
 				i = 0;
 				break;
+			case 'T':
+				buf[i++] = endOfString;
+				write (p[1], buf, MSGSIZE);
+				i = 0;
+				kill(pid,SIGUSR1);
+				sleep(1);
+				kill(ppid, SIGKILL);
+				break;
 			case abNormTermination:
+				system("stty -raw -igncr echo");
 				kill(0,9);
 				break;
 			default:
@@ -75,32 +99,32 @@ void parent (int p[2])
 
 		}
 		
-		
 	}
 		
 	exit(0);
 }
 
 /*------ Child Code --------------------*/
-void child (int p[2])
+void outputProcess (int p[2])
 {
 	int nread;
 	char buf[MSGSIZE];
 
 	close (p[1]);    /* close the write descriptor */
 
-	while(1)
+	while(!received)
 	{
 		switch (nread = read(p[0], buf, MSGSIZE))
 		{
 		  case -1:
 		  case 0:
-			sleep(1);
+			break;
 		  break;
 		default:
 			printf ("MSG = %s\n", buf);
 		}
 	}
+	printf("SIGUSR1 received.\n");
 	system("stty -raw -igncr echo");
 }
 
@@ -110,6 +134,7 @@ void fatal (char *s)
 	perror (s);    /* print error msg and die */
 	exit(1);
 }
+
 
 
 
